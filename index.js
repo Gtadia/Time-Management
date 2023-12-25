d490705556178fa66769828608928fa3f328ac87
Min Han <min@Mins-MacBook-Pro.local>
2023-12-24 20:45:57 -0500

    adding essentail files for react

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // We're going to use react (and react native) for the UI
 // That means that we're gonna have to learn how to use Redux
+
+// REMEMBER, the index.js folder can also go in the src file (just update the PATH accordingly)
+    // So essentially, it can go anywhere as long as the file paths are corrected for it
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './src/App.js';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App/>);
